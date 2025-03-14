2a1cd15e3f381559134fda428f60cc30b32da339
rumunyana <113432535+rumunyana@users.noreply.github.com>
2025-03-14 14:32:54 +0100

    Update wsgi.py

--- recommender_system/recommender_system/wsgi.py
+++ recommender_system/recommender_system/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'recommender_system.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'recommender_system.recommender_system.settings')
 
 application = get_wsgi_application()
